87b368136828d4fa9c4dacbb4d043c03360e5c98
sammydai <sammydaiwenting@163.com>
2020-10-20 13:24:30 +0800

    feature:新增mybatis模块

---
+++ springboot-jwt/src/main/java/com/dwt/springbootmybatis/domain/Dept.java
@@ -0,0 +1,56 @@
+package com.dwt.springbootmybatis.domain;
+
+/**
+ * @Package: com.dwt.springbootmybatis.domain
+ * @Description: Department
+ * @Author: Sammy
+ * @Date: 2020/9/23 13:33
+ */
+
+public class Dept {
+	private Integer deptId;
+	private String deptName;
+	private String deptLocation;
+	private String deptDescription;
+	private String deptManager;
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public String getDeptLocation() {
+		return deptLocation;
+	}
+
+	public void setDeptLocation(String deptLocation) {
+		this.deptLocation = deptLocation;
+	}
+
+	public String getDeptDescription() {
+		return deptDescription;
+	}
+
+	public void setDeptDescription(String deptDescription) {
+		this.deptDescription = deptDescription;
+	}
+
+	public String getDeptManager() {
+		return deptManager;
+	}
+
+	public void setDeptManager(String deptManager) {
+		this.deptManager = deptManager;
+	}
+
+	public Integer getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(Integer deptId) {
+		this.deptId = deptId;
+	}
+}
---
+++ springboot-jwt/src/main/java/com/dwt/springbootmybatis/domain/User.java
@@ -0,0 +1,70 @@
+package com.dwt.springbootmybatis.domain;
+
+import java.util.Date;
+
+/**
+ * @Package: com.dwt.springbootmybatis.domain
+ * @Description: User 测试类
+ * @Author: Sammy
+ * @Date: 2020/9/23 10:04
+ */
+
+public class User {
+	private Integer id;
+	private String username;
+	private Integer age;
+	private String address;
+	private String dept;
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	private Date createtime;
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+}
