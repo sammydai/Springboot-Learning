7f64c173e6fbaaf8a492841edf9c5ce8e537a281
sammydai <sammydaiwenting@163.com>
2020-12-21 10:53:32 +0800

    feature:log aop

--- springboot-log-aop/src/main/java/com/learning/service/AppService.java
+++ springboot-log-aop/src/main/java/com/learning/service/AppService.java
@@ -2,7 +2,6 @@ package com.learning.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
