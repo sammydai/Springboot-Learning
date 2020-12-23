24bfb8d83a2118c4b2e17838511ce35e77d5ebd5
sammydai <sammydaiwenting@163.com>
2020-12-23 13:10:22 +0800

    feature:service provider

--- springboot-zookeeper-provider/src/main/java/com/learning/zkprovider/controller/ZKProviderController.java
+++ springboot-zookeeper-provider/src/main/java/com/learning/zkprovider/controller/ZKProviderController.java
@@ -1,6 +1,8 @@
 package com.learning.zkprovider.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +11,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
- *@Package: com.learning.zkprovider.controller;
+ *@Package: com.learning.zkprovider.health;
  *@Description: ZKProviderController
  *@Author: Sammy
  *@Date: 2020/12/16 17:07
  *
  **/
 @RestController
+@Slf4j
 public class ZKProviderController {
     private AtomicInteger count = new AtomicInteger();
     private AtomicInteger sleepCount = new AtomicInteger();
@@ -35,8 +38,19 @@ public class ZKProviderController {
         }
 		int newCount = sleepCount.incrementAndGet();
         return "ribbon sleep count" + newCount + ": " + ThreadLocalRandom.current().nextInt(1000)+name;
-
 	}
 
+	@GetMapping(value = "hello/{name}")
+    public String hello(@PathVariable("name") String name, Integer mills) {
+        log.info("开始执行请求，name: " + name + "要求暂停：" + mills + "毫秒");
+        if (mills != null && mills > 0) {
+            try {
+                Thread.sleep(mills);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return "hello, [" + name + "], this is service producer by nacos.....";
+    }
 }
 
