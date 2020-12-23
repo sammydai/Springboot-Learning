8b38bc2f9e4f9198a61451711eaa4de9a958cce1
sammydai <sammydaiwenting@163.com>
2020-12-23 13:13:26 +0800

    feature:optimize import

--- springboot-exception-handler/src/main/java/com/learning/exceptionhandler/controller/ThreadPoolController.java
+++ springboot-exception-handler/src/main/java/com/learning/exceptionhandler/controller/ThreadPoolController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
